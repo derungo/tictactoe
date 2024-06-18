cd1b2c464fbe4860ea02d8e0f2cfef4537128aae
Patrick <patrick.aiken@gmail.com>
2024-06-17 21:21:22 -0400

    redid video reversal playlist

--- lib/video_player_widget.dart
+++ lib/video_player_widget.dart
@@ -10,20 +10,40 @@ class VideoBackground extends StatefulWidget {
 
 class _VideoBackgroundState extends State<VideoBackground> {
   late VideoPlayerController _controller;
+  late List<String> _videoAssets;
+  int _currentVideoIndex = 0;
 
   @override
   void initState() {
     super.initState();
-    _controller = VideoPlayerController.asset('assets/animation_sky_grass_bubbles.mp4')
+    _videoAssets = [
+      'assets/animation_sky_grass_bubbles.mp4',
+      'assets/animation_sky_grass_bubbles_reversed.mp4',
+    ];
+    _initializeAndPlay(_videoAssets[_currentVideoIndex]);
+  }
+
+  void _initializeAndPlay(String asset) {
+    _controller = VideoPlayerController.asset(asset)
       ..initialize().then((_) {
         setState(() {});
-        _controller.setLooping(true);
+        _controller.setLooping(false);
         _controller.play();
+        _controller.addListener(_videoListener);
       });
+  }
+
+  void _videoListener() {
+    if (_controller.value.position >= _controller.value.duration) {
+      _currentVideoIndex = (_currentVideoIndex + 1) % _videoAssets.length;
+      _controller.removeListener(_videoListener);
+      _initializeAndPlay(_videoAssets[_currentVideoIndex]);
+    }
   }
 
   @override
   void dispose() {
+    _controller.removeListener(_videoListener);
     _controller.dispose();
     super.dispose();
   }
